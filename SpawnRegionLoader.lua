cd5b2111254ae52916b6847d2f66f78cd9dff6f9
eltrul <105537798+eltrul@users.noreply.github.com>
2025-02-16 16:28:50 +0700

    Update SpawnRegionLoader.lua

--- SpawnRegionLoader.lua
+++ SpawnRegionLoader.lua
@@ -1,10 +1,10 @@
 local RawMobRegions = game:HttpGet("https://github.com/Bocchi-World/Bocchi_BF/raw/refs/heads/main/RawMobPositions.json") 
 
-ServerRuntime.MobRegions = {} 
+ScriptStorage.MobRegions = {} 
 
 for Name, Positions in Services.HttpService:JSONDecode(RawMobPositions) do 
-    ServerRuntime.MobRegions[Name] = {} 
+    ScriptStorage.MobRegions[Name] = {} 
     for _, Position in Positions do 
-        table.insert(ServerRuntime.MobRegions[Name], Vector3.new(Position[1], Position[2], Position[3])) 
+        table.insert(ScriptStorage.MobRegions[Name], Vector3.new(Position[1], Position[2], Position[3])) 
     end 
 end 
