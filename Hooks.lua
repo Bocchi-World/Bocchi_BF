487862e55080dd66644447729f03748764743f3d
eltrul <105537798+eltrul@users.noreply.github.com>
2025-02-16 12:03:39 +0700

    Update Hooks.lua

--- Hooks.lua
+++ Hooks.lua
@@ -1,26 +1,30 @@
+local Exports = {
+    Listeners = {}
+} 
+
 LastNotification = Instance.new("StringValue", workspace)
 
 LastNotification = "LastNotification"
 
-LastNotification.Changed:Connect(function()
-
+LastNotification.Changed:Connect(function(Content)
+    for ListenerContent, Callback in Exports.Listeners do 
+        if string.find(string.lower(Content), string.lower(ListenerContent)) then 
+            Callback(Content)
+        end 
+    end 
 end) 
 
+function Exports:RegisterNotifyListener(Senque, Callback)
+    Exports.Listeners[Senque] = Callback
+end 
 
 local old;
 
 old = hookfunction(
-
-                require(ReplicatedStorage.Notification).new,
-
-                function(a, b)
-
-                    v21 = tostring(tostring(a or "") .. tostring(b or "")) or ""
-
-                    workspace.LastNotification.Value = v21
-
-                    return old(a, b)
-
-                end
-
+    require(ReplicatedStorage.Notification).new,
+    function(a, b)
+        v21 = tostring(tostring(a or "") .. tostring(b or "")) or ""
+        workspace.LastNotification.Value = v21
+        return old(a, b)
+    end
 )
